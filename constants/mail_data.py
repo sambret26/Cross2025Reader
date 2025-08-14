205408b92b13edd7f33ff96c9d133ba622dcef54
sambret26 <samuelbretiere@orange.fr>
2025-08-14 19:46:36 +0200

    Update mail adress

--- constants/mail_data.py
+++ constants/mail_data.py
@@ -1,6 +1,6 @@
 MAIL_BODY = "En piece jointe le tableau des recompenses"
 MAIL_SUBJECT = "Recompenses Cross Oriol 2025"
 MAIL_FROM = "samuelbretiere@orange.fr"
-MAIL_TO = "samuelbretiere@orange.fr"
+MAIL_TO = "famillebret.26@orange.fr"
 SMTP = "smtp.orange.fr"
 PORT = 465
